5945c90f2c40e4511491f3dd642fb88f5de043a7
Long <ln24630@gmail.com>
2019-04-21 00:21:32 -0700

    hangman ver 1

--- beachhacks/beachhacks2019/hangman.py
+++ beachhacks/beachhacks2019/hangman.py
@@ -1,1 +1,34 @@
 #Hangman
+
+def main():
+    user_input = -1
+    while user_input != 2:
+        while user_input < 1 or user_input > 2:
+            print_menu()
+            user_input = int(input('Enter a selection: '))
+        if user_input == 1:
+            hangman_menu()
+        elif user_input == 2:
+            print("Goodbye")
+            break
+        user_input = -1
+
+def print_menu():
+    pass
+
+def word_generator():
+    pass
+
+def get_user_chacter():
+    pass
+
+def matching_word(user_input):
+    pass
+
+def hangman_menu():
+    pass
+
+def print_word():
+    pass
+        
+        
